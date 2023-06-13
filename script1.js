cfa5c65f8609ac56538e4c7ffac0c9cffb423cba
EmanueleManno <mannus1994@gmail.com>
2023-06-13 15:00:07 +0200

    Stampo la lista con nome tavolo e segnaposto

--- script1.js
+++ script1.js
@@ -1,61 +1,72 @@
 //VEDO SE FUNZIONA:
 console.log('JS OK');
 
-//CREAZIONE DELL'ARRAY DEGLI INVITATI:
-const invitati = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
-
-//STAMPA DEGLI INVITATI
-for (let i = 0; i < invitati.length; i++) {
-    const invitato = invitati[i];
-    console.log(invitato);
-}
-
 //IMPOSTO IL NOME DEL TAVOLO:
 const nomeTavolo = 'Tavolo Vip';
 
-//CREAZIONE DELL'ARRAY DEGLI INVITATI:
+//IMPOSTO IL NUMERO DEL SEGNAPOSTO A ZERO:
+let numeroSegnaposto = 0;
+
+//CREAZIONE DELL'ARRAY DEGLI INVITATI COL TAVOLO E IL SEGNAPOSTO:
 const invitatiConTavolo = [ 
     {
         nome: 'Brad Pitt',
         tavolo: (nomeTavolo),
+        segnaposto: numeroSegnaposto,
     }, 
     {
         nome: 'Johnny Depp',
         tavolo: (nomeTavolo),
+        segnaposto: numeroSegnaposto,
     },
     {
         nome: 'Lady Gaga',
         tavolo: (nomeTavolo),
+        segnaposto: numeroSegnaposto,
     },
     {
         nome: 'Cristiano Ronaldo',
         tavolo: (nomeTavolo),
+        segnaposto: numeroSegnaposto,
     },
     {
         nome: 'Georgina Rodriguez',
         tavolo: (nomeTavolo),
+        segnaposto: numeroSegnaposto,
     },
     {
         nome: 'Chiara Ferragni',
         tavolo: (nomeTavolo),
+        segnaposto: numeroSegnaposto,
     },
     {
         nome: 'Fedez',
         tavolo: (nomeTavolo),
+        segnaposto: numeroSegnaposto,
     },
     {
         nome: 'George Clooney',
         tavolo: (nomeTavolo),
+        segnaposto: numeroSegnaposto,
     },
     {
         nome: 'Amal Clooney',
         tavolo: (nomeTavolo),
+        segnaposto: numeroSegnaposto,
     },
     {
         nome: 'Maneskin',
         tavolo: (nomeTavolo),
+        segnaposto: numeroSegnaposto,
     }
 ];
 
-//STAMPO IN CONSOLE LA LISTA DEGLI INVITATI COL TAVOLO:
+//CALCOLO IL SEGNAPOSTO:
+invitatiConTavolo.forEach(element => {
+    numeroSegnaposto = invitatiConTavolo.indexOf(element) + 1
+    element.segnaposto = numeroSegnaposto;
+    console.log(element)
+})
+
+//STAMPO IN CONSOLE LA LISTA DEGLI INVITATI COL TAVOLO E IL SEGNAPOSTO:
 console.table(invitatiConTavolo);
