f1b18a8943ff374fecb9916aa841ab937e30c8f6
EmanueleManno <mannus1994@gmail.com>
2023-06-13 15:04:50 +0200

    Creazione array studenti

--- script2.js
+++ script2.js
@@ -1,2 +1,47 @@
 //VEDO SE FUNZIONA:
 console.log('JS OK');
+
+//CREAZIONE DELL'ARRAY DEGLI STUDENTI:
+const students = [
+    {
+        id: 213,
+        name: "Marco della Rovere",
+        grade: 78
+    },
+
+    {
+        id: 110,
+        name: "Paola Cortellessa",
+        grade: 96
+    },
+
+    {
+        id: 250,
+        name: "Andrea Mantegna",
+        grade: 48
+    },
+
+    {
+        id: 145,
+        name: "Gaia Borromini",
+        grade: 74
+    },
+
+    {
+        id: 196,
+        name: "Luigi Grimaldello",
+        grade: 68
+    },
+
+    {
+        id: 102,
+        name: "Piero della Francesca",
+        grade: 50
+    },
+
+    {
+        id: 120,
+        name: "Francesca da Polenta",
+        grade: 84
+    }
+]
