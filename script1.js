42425e80b7d82999c057891dab0c5b92f9fb5cd5
EmanueleManno <mannus1994@gmail.com>
2023-06-13 14:41:53 +0200

    Creazione dell'array e stampa degli invitati

--- script1.js
+++ script1.js
@@ -1,2 +1,11 @@
 //VEDO SE FUNZIONA:
 console.log('JS OK');
+
+//CREAZIONE DELL'ARRAY DEGLI INVITATI:
+const invitati = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+
+//STAMPA DEGLI INVITATI
+for (let i = 0; i < invitati.length; i++) {
+    const invitato = invitati[i];
+    console.log(invitato);
+}
