4b4951d3b1a3ebfe20a2ac724443d265072565c3
EmanueleManno <mannus1994@gmail.com>
2023-06-13 14:50:55 +0200

    Stampo gli invitati col Tavolo Vip

--- script1.js
+++ script1.js
@@ -9,3 +9,53 @@ for (let i = 0; i < invitati.length; i++) {
     const invitato = invitati[i];
     console.log(invitato);
 }
+
+//IMPOSTO IL NOME DEL TAVOLO:
+const nomeTavolo = 'Tavolo Vip';
+
+//CREAZIONE DELL'ARRAY DEGLI INVITATI:
+const invitatiConTavolo = [ 
+    {
+        nome: 'Brad Pitt',
+        tavolo: (nomeTavolo),
+    }, 
+    {
+        nome: 'Johnny Depp',
+        tavolo: (nomeTavolo),
+    },
+    {
+        nome: 'Lady Gaga',
+        tavolo: (nomeTavolo),
+    },
+    {
+        nome: 'Cristiano Ronaldo',
+        tavolo: (nomeTavolo),
+    },
+    {
+        nome: 'Georgina Rodriguez',
+        tavolo: (nomeTavolo),
+    },
+    {
+        nome: 'Chiara Ferragni',
+        tavolo: (nomeTavolo),
+    },
+    {
+        nome: 'Fedez',
+        tavolo: (nomeTavolo),
+    },
+    {
+        nome: 'George Clooney',
+        tavolo: (nomeTavolo),
+    },
+    {
+        nome: 'Amal Clooney',
+        tavolo: (nomeTavolo),
+    },
+    {
+        nome: 'Maneskin',
+        tavolo: (nomeTavolo),
+    }
+];
+
+//STAMPO IN CONSOLE LA LISTA DEGLI INVITATI COL TAVOLO:
+console.table(invitatiConTavolo);
