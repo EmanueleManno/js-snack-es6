0cb9eb9ee6a17fa78e854a6a8172738f4dbf7af7
EmanueleManno <mannus1994@gmail.com>
2023-06-14 14:20:00 +0200

    Creazione dell'array di oggetti delle bici

--- script3.js
+++ script3.js
@@ -1,2 +1,31 @@
 //VEDIAMO SE FUNZIONA
 console.log('JS OK');
+
+//CREO L'ARRAY DI BICICLETTE:
+biciclette [
+    {
+        nome: 'Bici da città',
+        peso: 200
+    },
+    {
+        nome: 'Mountain bike',
+        peso: 350
+    },
+    {
+        nome: 'Bici da corsa',
+        peso: 75
+    },
+    {
+        nome: 'Bici ibrida',
+        peso: 115
+    },
+    {
+        nome: 'Bici pieghevole',
+        peso: 120
+    },
+    {
+        nome: 'Bici da cicloturismo',
+        peso: 250
+    },
+
+]
